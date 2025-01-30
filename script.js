310dfeb99defcc71e2aa4b16f4902e9e074855f6
Ariski <Ariski@ARISKI>
2025-01-30 07:31:55 +0700

    menambahkan script.js dan function addtodo()

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('form');
+    submitForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      addTodo();
+    });
+  });
+
+function addTodo() {
+  const textTodo = document.getElementById('title').value;
+  const timestamp = document.getElementById('date').value;
+ 
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+  todos.push(todoObject);
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
